041a81eb82f9ca347fadac9098c88934c81a467c
lapwat <q.lapointe@gmail.com>
2020-04-24 03:52:10 +0000

    remove duplicated sed command

--- mod.sh
+++ mod.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-# clean: can be removed if serverless?
-rm -rf *.apk NfcNci/ NQNfcNci/ NxpNfcNci/
+# create a temporary directory
+TMP_DIR=$(mktemp -d -t nso-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXX)
 
 # unzip
-unzip apks.zip
+unzip apks.zip -d "$TMP_DIR/"
+
+cd "$TMP_DIR/"
 
 set 'NfcNci' 'NQNfcNci' 'NxpNfcNci'
 for name do
@@ -19,7 +21,6 @@ apktool d -f "$APK_NAME.apk" -o "$APK_NAME/"
 
 # mod
 sed -e '/.*if-lt.*/s/^/#/g' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
-sed -e '/.*if-lt.*/s/^/#/g' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
 sed 's/SCREEN_OFF/SCREEN_OFFA/' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
 sed 's/SCREEN_ON/SCREEN_ONA/' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
 sed 's/USER_PRESENT/USER_PRESENTA/' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
@@ -30,3 +31,6 @@ apktool b -f "$APK_NAME/" -o "${APK_NAME}_mod.apk"
 # need if serverless?: keytool -genkey -v -keystore ~/.android/debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
 #jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -storepass android "${APK_NAME}_mod.apk" androiddebugkey
 zipalign -v 4 "${APK_NAME}_mod.apk" "${APK_NAME}_align.apk"
+
+cd -
+cp "$TMP_DIR/${APK_NAME}_align.apk" .
