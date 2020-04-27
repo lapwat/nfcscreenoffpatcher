3674dad163db2ce84836e322a47754b54b535776
lapwat <q.lapointe@gmail.com>
2020-04-27 19:21:00 +0200

    save uploads in data folder

--- mod.sh
+++ mod.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
-
-# retrieve temporary directory
-#TMP_DIR=$(mktemp -d -t nso-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXX)
-#echo -n "$TMP_DIR"
-TMP_DIR=$1
-
-# unzip
-unzip apks.zip -d "$TMP_DIR/"
-
-cd "$TMP_DIR/"
-
-set 'NfcNci' 'NQNfcNci' 'NxpNfcNci'
-for name do
-  if [ -f "$name.apk" ]; then
-    APK_NAME="$name"
-  fi
-done
+cd "$EXTRACT_DIR/"
 
 # decompile
 apktool if framework-res.apk
@@ -30,10 +14,6 @@ sed 's/USER_SWITCHED/USER_SWITCHEDA/' -i "$APK_NAME/smali/com/android/nfc/NfcSer
 
 # build
 apktool b -f "$APK_NAME/" -o "${APK_NAME}_mod.apk"
-# need if serverless?: keytool -genkey -v -keystore ~/.android/debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
-#jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -storepass android "${APK_NAME}_mod.apk" androiddebugkey
+# keytool -genkey -v -keystore ~/.android/debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
+# jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -storepass android "${APK_NAME}_mod.apk" androiddebugkey
 zipalign -v 4 "${APK_NAME}_mod.apk" "${APK_NAME}_align.apk"
-
-cd -
-cp "$TMP_DIR/${APK_NAME}_align.apk" out.apk 
-
--- server.py
+++ server.py
@@ -1,135 +1,36 @@
-#!/usr/bin/env python3
- 
-"""Simple HTTP Server With Upload.
+import os, subprocess, zipfile
+import http.server as server
+from datetime import datetime
 
-This module builds on http.server by implementing the standard GET
-and HEAD requests in a fairly straightforward manner.
+class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
+  def do_PUT(self):
+    # save archive
+    filename = os.path.basename(self.path)
+    apk_name = os.path.splitext(filename)[0]
+    file_length = int(self.headers['Content-Length'])
+    with open(filename, 'wb') as output_file:
+        output_file.write(self.rfile.read(file_length))
 
-see: https://gist.github.com/UniIsland/3346170
-"""
- 
- 
-__version__ = "0.1"
-__all__ = ["SimpleHTTPRequestHandler"]
-__author__ = "bones7456"
-__home_page__ = "https://gist.github.com/UniIsland/3346170"
- 
-import os
-import posixpath
-import http.server
-import socketserver
-import urllib.request, urllib.parse, urllib.error
-import html
-import shutil
-import mimetypes
-import re
-import argparse
-import base64
-import random
+    # unzip
+    extract_dir = f'/data/{int(datetime.timestamp(datetime.now()))}'
+    with zipfile.ZipFile(filename) as archive:
+      archive.extractall(extract_dir)
+    
+    # mod
+    subprocess.run(['./mod.sh'], env={ 'EXTRACT_DIR': extract_dir, 'APK_NAME': apk_name })
 
-from io import BytesIO
+    # check that apk has been successfully disassembled
+    if not os.path.exists(f'{extract_dir}/{apk_name}/smali'):
+      self.send_error(500)
+      return
 
-from pathlib import Path
-from subprocess import check_output
+    # write aligned apk in response
+    self.send_response(200)
+    with open(f'{extract_dir}/{apk_name}_align.apk', 'rb') as apk:
+      self.send_header("Content-Type", 'application/vnd.android.package-archive')
+      fs = os.fstat(apk.fileno())
+      self.send_header("Content-Length", str(fs[6]))
+      self.end_headers()
+      self.wfile.write(apk.read())
 
-alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-r = random.SystemRandom()
-
-class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    server_version = "SimpleHTTPWithUpload/" + __version__
-    #tmp_dir = ''.join([r.choice(alphabet) for _ in range(24)])
-
-    def do_POST(self):
-        r, info = self.deal_post_data()
-        print((r, info, "by: ", self.client_address))
-
-        # moding in tmp directory
-        tmp_dir = check_output(['mktemp', '-d', '-t', 'nso-XXXXXXXX']).decode().strip()
-        check_output(['./mod.sh', tmp_dir])
-
-        # checking that apk has been successfully disassembled
-        if not os.path.exists(f'{tmp_dir}/NfcNci/smali') and not os.path.exists(f'{tmp_dir}/NQNfcNci/smali') and not os.path.exists(f'{tmp_dir}/NxpNfcNci/smali'):
-            self.send_error(500)
-            f.close()
-            return	
-
-        # write aligned apk in response
-        f = self.send_head()
-        shutil.copyfileobj(f, self.wfile)
-        f.close()
-        
-    def deal_post_data(self):
-        uploaded_files = []
-        content_type = self.headers['content-type']
-        if not content_type:
-            return (False, "Content-Type header doesn't contain boundary")
-        boundary = content_type.split("=")[1].encode()
-        remainbytes = int(self.headers['content-length'])
-        line = self.rfile.readline()
-        remainbytes -= len(line)
-        if not boundary in line:
-            return (False, "Content NOT begin with boundary")
-        while remainbytes > 0:
-            line = self.rfile.readline()
-            remainbytes -= len(line)
-            fn = f'apks.zip'
-            # Path(self.tmp_dir).mkdir(parents=True, exist_ok=True)
-            line = self.rfile.readline()
-            remainbytes -= len(line)
-            line = self.rfile.readline()
-            remainbytes -= len(line)
-            try:
-                out = open(fn, 'wb')
-            except IOError:
-                return (False, "Can't create file to write, do you have permission to write?")
-            else:
-                with out:                    
-                    preline = self.rfile.readline()
-                    remainbytes -= len(preline)
-                    while remainbytes > 0:
-                        line = self.rfile.readline()
-                        remainbytes -= len(line)
-                        if boundary in line:
-                            preline = preline[0:-1]
-                            if preline.endswith(b'\r'):
-                                preline = preline[0:-1]
-                            out.write(preline)
-                            uploaded_files.append(fn)
-                            break
-                        else:
-                            out.write(preline)
-                            preline = line
-        return (True, "File '%s' upload success!" % ",".join(uploaded_files))
- 
-    def send_head(self):
-        f = open(f'out.apk', 'rb')
-        self.send_response(200)
-        self.send_header("Content-type", 'application/zip')
-        fs = os.fstat(f.fileno())
-        self.send_header("Content-Length", str(fs[6]))
-        self.end_headers()
-        return f
- 
-parser = argparse.ArgumentParser()
-parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
-                        help='Specify alternate bind address '
-                             '[default: all interfaces]')
-parser.add_argument('port', action='store',
-                        default=8000, type=int,
-                        nargs='?',
-                        help='Specify alternate port [default: 8000]')
-args = parser.parse_args()
-
-PORT = args.port
-BIND = args.bind
-HOST = BIND
-
-if HOST == '':
-	HOST = 'localhost'
-
-Handler = SimpleHTTPRequestHandler
-
-with socketserver.TCPServer((BIND, PORT), Handler) as httpd:
-	serve_message = "Serving HTTP on {host} port {port} (http://{host}:{port}/) ..."
-	print(serve_message.format(host=HOST, port=PORT))
-	httpd.serve_forever()
+server.test(HandlerClass=HTTPRequestHandler)
