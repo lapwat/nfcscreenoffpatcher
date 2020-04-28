05d64fe26f4a2256c286f6b5515459ac3c1345fe
lapwat <q.lapointe@gmail.com>
2020-04-28 16:28:05 +0000

    authorize hce when screen is off

--- mod.sh
+++ mod.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 EXTRACT_DIR=$1
 APK_NAME=$2
+SMALI_PATH="$APK_NAME/smali/com/android/nfc/NfcService.smali"
 
 cd "$EXTRACT_DIR/"
 
@@ -9,11 +10,12 @@ apktool if framework-res.apk
 apktool d -f "$APK_NAME.apk" -o "$APK_NAME/"
 
 # mod
-sed -e '/.*if-lt.*/s/^/#/g' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
-sed 's/SCREEN_OFF/SCREEN_OFFA/' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
-sed 's/SCREEN_ON/SCREEN_ONA/' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
-sed 's/USER_PRESENT/USER_PRESENTA/' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
-sed 's/USER_SWITCHED/USER_SWITCHEDA/' -i "$APK_NAME/smali/com/android/nfc/NfcService.smali"
+sed -e '/.*if-lt.*/s/^/#/g' -i "$SMALI_PATH"
+grep -n -A4 -B4 if-lt "$SMALI_PATH" | tail -9 | sed -e 's/[^0-9].*$//' -e 's/$/s|^|#|/' | sed -i -f- "$SMALI_PATH"
+sed 's/SCREEN_OFF/SCREEN_OFF_DISABLED/' -i "$SMALI_PATH"
+sed 's/SCREEN_ON/SCREEN_ON_DISABLED/' -i "$SMALI_PATH"
+sed 's/USER_PRESENT/USER_PRESENT_DISABLED/' -i "$SMALI_PATH"
+sed 's/USER_SWITCHED/USER_SWITCHED_DISABLED/' -i "$SMALI_PATH"
 
 # build
 apktool b -f "$APK_NAME/" -o "${APK_NAME}_mod.apk"
